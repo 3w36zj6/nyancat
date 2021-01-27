e73be84c04eae9db246a0916142abb24cb93331c
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2021-01-27 16:07:13 +0900

    NyanCat追加

--- mygame.py
+++ mygame.py
@@ -3,8 +3,9 @@ import pyxel
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120, caption="NyanCat")
-        pyxel.image(0).load(0, 0, "assets/cat_16x16.png")
+        pyxel.init(width=160, height=120, caption="NyanCat")
+        pyxel.load(filename="assets/cat.pyxres")
+        pyxel.image(1).load(x=0, y=0, filename="assets/cat_16x16.png")
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -13,7 +14,8 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.blt(x=75, y=45, img=0, u=0, v=0, w=16, h=16, colkey=13)
+        pyxel.blt(x=75, y=45, img=1, u=0, v=0, w=16, h=16, colkey=13)
+        pyxel.blt(x=10, y=10, img=0, u=0, v=0, w=64, h=64, colkey=5)
 
 
 App()
