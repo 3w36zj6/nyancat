467a0afa46372f581c9f05197f9ea4d60ecc5223
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2021-02-16 04:35:25 +0900

    虹実装

--- mygame.py
+++ mygame.py
@@ -1,4 +1,5 @@
 import pyxel
+from collections import deque
 
 
 class Player:
@@ -6,13 +7,18 @@ class Player:
         self.x = x
         self.y = y
         self.frame = 0
+        self.animation_frame = 0
 
     def update(self):
+        # Rainbow
+        is_moving_right = False
+
         if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD_1_LEFT):
             self.x = max(self.x - 2, 0)
 
         if pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD_1_RIGHT):
             self.x = min(self.x + 2, pyxel.width - 40)
+            is_moving_right = True
 
         if pyxel.btn(pyxel.KEY_UP) or pyxel.btn(pyxel.GAMEPAD_1_UP):
             self.y = max(self.y - 2, 0)
@@ -20,11 +26,50 @@ class Player:
         if pyxel.btn(pyxel.KEY_DOWN) or pyxel.btn(pyxel.GAMEPAD_1_DOWN):
             self.y = min(self.y + 2, pyxel.height - 24)
 
+        Rainbow.append_rainbow(is_moving_right=is_moving_right,
+                               x=self.x + 12, y=self.y + (self.animation_frame >= 3))
+        self.animation_frame = self.frame // 3 % 6
         self.frame += 1
 
     def draw(self):
         pyxel.blt(x=self.x, y=self.y, img=0, u=0, v=24 *
-                  (self.frame // 3 % 6), w=40, h=24, colkey=5)
+                  self.animation_frame, w=40, h=24, colkey=5)
+
+
+class Rainbow:
+    rainbows = deque()
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.frame = 0
+
+    def update(self):
+        self.x -= 3
+        self.frame += 1
+
+    def draw(self):
+        for i in range(3):
+            pyxel.blt(x=self.x + i, y=self.y, img=1,
+                      u=0, v=0, w=40, h=24, colkey=5)
+
+    @classmethod
+    def append_rainbow(cls, is_moving_right, x, y):
+        Rainbow.rainbows.append(Rainbow(x, y))
+        if is_moving_right:
+            Rainbow.rainbows.append(Rainbow(x-3, y))
+
+    @classmethod
+    def update_all(self):
+        for rainbow in Rainbow.rainbows.copy():
+            rainbow.update()
+            if rainbow.x < -16:
+                Rainbow.rainbows.popleft()
+
+    @classmethod
+    def draw_all(self):
+        for rainbow in Rainbow.rainbows:
+            rainbow.draw()
 
 
 class App:
@@ -38,7 +83,9 @@ class App:
         pyxel.run(self.update, self.draw)
 
     def update(self):
+        Rainbow.update_all()
         self.player.update()
+
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
@@ -46,6 +93,7 @@ class App:
         pyxel.cls(0)
         pyxel.blt(x=75, y=45, img=1, u=0, v=0, w=40, h=24, colkey=5)
 
+        Rainbow.draw_all()
         self.player.draw()
 
 
