3a93d3b140b202d1f6b8b85a3bc7b1f52c38270f
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2021-08-13 04:33:14 +0900

    fix

--- nyancat.py
+++ nyancat.py
@@ -19,40 +19,69 @@ class Player:
         is_moving_right = False
 
         if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD_1_LEFT):
-            Player.player.x = max(Player.player.x - 2, 0)
+            cls.player.x = max(cls.player.x - 2, 0)
 
         if pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD_1_RIGHT):
-            Player.player.x = min(Player.player.x + 2,
-                                  pyxel.width - Player.width)
+            cls.player.x = min(cls.player.x + 2,
+                               pyxel.width - cls.width)
             is_moving_right = True
 
         if pyxel.btn(pyxel.KEY_UP) or pyxel.btn(pyxel.GAMEPAD_1_UP):
-            Player.player.y = max(Player.player.y - 2, 0)
+            cls.player.y = max(cls.player.y - 2, 0)
 
         if pyxel.btn(pyxel.KEY_DOWN) or pyxel.btn(pyxel.GAMEPAD_1_DOWN):
-            Player.player.y = min(Player.player.y + 2,
-                                  pyxel.height - Player.height)
+            cls.player.y = min(cls.player.y + 2,
+                               pyxel.height - cls.height)
 
         if pyxel.btnp(pyxel.KEY_SPACE):
-            Bullet.append_bullet(Player.player.x + Player.width - 16,
-                                 Player.player.y + Player.height//2 - 16//2)
+            Bullet.append(cls.player.x + cls.width - 16,
+                          cls.player.y + cls.height//2 - 16//2)
 
-        Rainbow.append_rainbow(is_moving_right=is_moving_right,
-                               x=Player.player.x + 12, y=Player.player.y + (Player.player.animation_frame >= 3))
-        Player.player.animation_frame = Player.player.frame // 3 % 6
-        Player.player.frame += 1
+        Rainbow.append(is_moving_right=is_moving_right,
+                       x=cls.player.x + 12, y=cls.player.y + (cls.player.animation_frame >= 3))
+        cls.player.animation_frame = cls.player.frame // 3 % 6
+        cls.player.frame += 1
 
     @classmethod
     def draw(cls):
-        pyxel.blt(x=Player.player.x, y=Player.player.y, img=0, u=0, v=Player.height *
-                  Player.player.animation_frame, w=Player.width, h=Player.height, colkey=5)
+        pyxel.blt(x=cls.player.x, y=cls.player.y, img=0, u=0, v=cls.height *
+                  cls.player.animation_frame, w=cls.width, h=cls.height, colkey=5)
 
     @classmethod
     def setup(cls, x, y):
-        Player.player = Player(x, y)
+        cls.player = cls(x, y)
 
 
-class Rainbow:
+class Sprite:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    @classmethod
+    def setup(cls):
+        cls.sprites = deque()
+
+    @classmethod
+    def append(cls):
+        pass
+
+    @classmethod
+    def update_all(cls):
+        for sprite in cls.sprites:
+            sprite.update()
+
+    @classmethod
+    def draw_all(cls):
+        for sprite in cls.sprites:
+            sprite.draw()
+
+
+class Rainbow(Sprite):
     width = 1
     height = 24
 
@@ -61,40 +90,30 @@ class Rainbow:
         self.y = y
         self.frame = 0
 
-    def __update(self):
+    def update(self):
         self.x -= 3
         self.frame += 1
 
-    def __draw(self):
+    def draw(self):
         for i in range(3):
             pyxel.blt(x=self.x + i, y=self.y, img=1,
                       u=0, v=0, w=Rainbow.width, h=Rainbow.height, colkey=1)
 
     @classmethod
-    def setup(cls):
-        Rainbow.rainbows = deque()
-
-    @classmethod
-    def append_rainbow(cls, is_moving_right, x, y):
-        Rainbow.rainbows.append(Rainbow(x, y))
+    def append(cls, is_moving_right, x, y):
+        cls.sprites.append(cls(x, y))
         if is_moving_right:
-            Rainbow.rainbows.append(Rainbow(x-3, y))
+            cls.sprites.append(cls(x-3, y))
 
     @classmethod
     def update_all(cls):
-        for rainbow in Rainbow.rainbows.copy():
-            rainbow.__update()
-            if rainbow.x < - 10:
-                Rainbow.rainbows.popleft()
-
-    @classmethod
-    def draw_all(cls):
-        for rainbow in Rainbow.rainbows:
-            rainbow.__draw()
+        for sprite in cls.sprites.copy():
+            sprite.update()
+            if sprite.x < - 10:
+                cls.sprites.popleft()
 
 
-class Star:
-    stars = deque()
+class Star(Sprite):
     frame = 0
     count = 0
     width = 7
@@ -106,43 +125,38 @@ class Star:
         self.frame = random.randint(0, 17)
         self.animation_frame = 0
 
-    def __update(self):
+    def update(self):
         self.x -= 3
         self.animation_frame = self.frame // 3 % 6
         self.frame += 1
 
-    def __draw(self):
+    def draw(self):
         pyxel.blt(x=self.x, y=self.y, img=1,
                   u=16, v=Star.height*self.animation_frame, w=Star.width, h=Star.height, colkey=1)
 
     @classmethod
-    def append_star(cls):
-        if Star.count % 2 == 0:
-            Star.stars.append(
-                Star(x=pyxel.width, y=random.randint(0, (pyxel.height - Star.height) // 2)))
+    def append(cls):
+        if cls.count % 2 == 0:
+            cls.sprites.append(
+                cls(x=pyxel.width, y=random.randint(0, (pyxel.height - cls.height) // 2)))
         else:
-            Star.stars.append(
-                Star(x=pyxel.width, y=random.randint((pyxel.height - Star.height) // 2, pyxel.height - Star.height)))
-        Star.count += 1
+            cls.sprites.append(
+                cls(x=pyxel.width, y=random.randint((pyxel.height - cls.height) // 2, pyxel.height - cls.height)))
+        cls.count += 1
 
     @classmethod
     def update_all(cls):
-        if Star.frame % 3 == 0:
-            Star.append_star()
-        for star in Star.stars.copy():
-            star.__update()
-            if star.x < -Star.width:
-                Star.stars.popleft()
+        if cls.frame % 3 == 0:
+            cls.append()
+        for sprite in cls.sprites.copy():
+            sprite.update()
+            if sprite.x < -cls.width:
+                cls.sprites.popleft()
 
-        Star.frame += 1
-
-    @classmethod
-    def draw_all(cls):
-        for star in Star.stars:
-            star.__draw()
+        cls.frame += 1
 
 
-class Bullet:
+class Bullet(Sprite):
     width = 16
     height = 16
 
@@ -152,40 +166,32 @@ class Bullet:
         self.frame = 0
         self.animation_frame = 0
 
-    def __update(self):
+    def update(self):
         self.x += 4
         self.animation_frame = self.frame // 2 % 4
         self.frame += 1
 
-    def __draw(self):
+    def draw(self):
         pyxel.blt(x=self.x, y=self.y, img=0,
                   u=40, v=Bullet.height*self.animation_frame, w=Bullet.width, h=Bullet.height, colkey=5)
 
     @classmethod
-    def setup(cls):
-        Bullet.bullets = deque()
-
-    @classmethod
-    def append_bullet(cls, x, y):
-        if len(Bullet.bullets) < 3:
-            Bullet.bullets.append(Bullet(x, y))
+    def append(cls, x, y):
+        if len(cls.sprites) < 3:
+            cls.sprites.append(cls(x, y))
 
     @classmethod
     def update_all(cls):
-        for bullet in Bullet.bullets.copy():
-            bullet.__update()
-            if bullet.x > pyxel.width:
-                Bullet.bullets.popleft()
-
-    @classmethod
-    def draw_all(cls):
-        for bullet in Bullet.bullets:
-            bullet.__draw()
+        for sprite in cls.sprites.copy():
+            sprite.update()
+            if sprite.x > pyxel.width:
+                cls.sprites.popleft()
 
 
-class Coin:
+class Coin(Sprite):
     width = 8
     height = 9
+    frame = 0
 
     def __init__(self, x, y):
         self.x = x
@@ -194,57 +200,48 @@ class Coin:
         self.frame = 0
         self.animation_frame = 0
 
-    def __update(self):
+    def update(self):
         self.x -= self.vx
         self.animation_frame = self.frame // 3 % 4
         self.frame += 1
 
-    def __draw(self):
+    def draw(self):
         pyxel.blt(x=int(self.x), y=self.y, img=1,
                   u=24, v=Coin.height*self.animation_frame, w=Coin.width, h=Coin.height, colkey=1)
 
     @classmethod
-    def setup(cls):
-        Coin.coins = deque()
-        Coin.frame = 0
-
-    @classmethod
-    def append_coin(cls):
-        Coin.coins.append(
-            Coin(pyxel.width, random.randint(0, pyxel.height - Coin.height)))
+    def append(cls):
+        cls.sprites.append(
+            cls(pyxel.width, random.randint(0, pyxel.height - cls.height)))
 
     @classmethod
     def update_all(cls):
-        if Coin.frame % 8 == 0:
-            Coin.append_coin()
-        for coin in Coin.coins.copy():
-            coin.__update()
-            if coin.x < -Coin.width:
-                Coin.coins.remove(coin)
+        if cls.frame % 8 == 0:
+            cls.append()
+        for sprite in cls.sprites.copy():
+            sprite.update()
+            if sprite.x < -cls.width:
+                cls.sprites.remove(sprite)
                 continue
 
-            if coin.x < Player.player.x + Player.width and Player.player.x < coin.x + Coin.width and coin.y < Player.player.y + Player.height and Player.player.y < coin.y + Coin.height:
+            if sprite.x < Player.player.x + Player.width and Player.player.x < sprite.x + cls.width and sprite.y < Player.player.y + Player.height and Player.player.y < sprite.y + cls.height:
                 if App.game_mode == 1:
                     App.score += 1
-                Coin.coins.remove(coin)
+                cls.sprites.remove(sprite)
                 continue
 
-            for bullet in Bullet.bullets:
-                if coin.x < bullet.x + Bullet.width and bullet.x < coin.x + Coin.width and coin.y < bullet.y + Bullet.height and bullet.y < coin.y + Coin.height:
-                    Coin.coins.remove(coin)
+            for bullet in Bullet.sprites:
+                if sprite.x < bullet.x + Bullet.width and bullet.x < sprite.x + cls.width and sprite.y < bullet.y + Bullet.height and bullet.y < sprite.y + cls.height:
+                    cls.sprites.remove(sprite)
                     break
 
-        Coin.frame += 1
-
-    @classmethod
-    def draw_all(cls):
-        for coin in Coin.coins:
-            coin.__draw()
+        cls.frame += 1
 
 
-class Bomb:
+class Bomb(Sprite):
     width = 16
     height = 16
+    frame = 0
 
     def __init__(self, x, y):
         self.x = x
@@ -253,63 +250,52 @@ class Bomb:
             (1 + (pyxel.frame_count - App.start_frame_count) / 600)
         self.frame = 0
 
-    def __update(self):
+    def update(self):
         self.x -= self.vx
         self.frame += 1
 
-    def __draw(self):
+    def draw(self):
         pyxel.blt(x=int(self.x), y=self.y, img=2,
                   u=0, v=0, w=Bomb.width, h=Bomb.height, colkey=1)
 
     @classmethod
-    def setup(cls):
-        Bomb.bombs = deque()
-        Bomb.frame = 0
-
-    @classmethod
-    def append_bomb(cls):
+    def append(cls):
         if App.game_mode == 1:
-            Bomb.bombs.append(
-                Bomb(pyxel.width, random.randint(0, pyxel.height - Bomb.height)))
+            cls.sprites.append(
+                cls(pyxel.width, random.randint(0, pyxel.height - cls.height)))
 
     @classmethod
     def update_all(cls):
-        if Bomb.frame % max(30 - (pyxel.frame_count - App.start_frame_count) // 600, 5) == 0:
-            Bomb.append_bomb()
-        for bomb in Bomb.bombs.copy():
-            bomb.__update()
-            if bomb.x < -Bomb.width:
-                Bomb.bombs.remove(bomb)
+        if cls.frame % max(30 - (pyxel.frame_count - App.start_frame_count) // 600, 5) == 0:
+            cls.append()
+        for sprite in cls.sprites.copy():
+            sprite.update()
+            if sprite.x < -cls.width:
+                cls.sprites.remove(sprite)
                 continue
 
-            for bullet in Bullet.bullets.copy():
-                if bomb.x < bullet.x + Bullet.width and bullet.x < bomb.x + Bomb.width and bomb.y < bullet.y + Bullet.height and bullet.y < bomb.y + Bomb.height:
-                    Bomb.bombs.remove(bomb)
-                    Bullet.bullets.remove(bullet)
-                    Explosion.append_explosion(bomb.x, bomb.y)
+            for bullet in Bullet.sprites.copy():
+                if sprite.x < bullet.x + Bullet.width and bullet.x < sprite.x + cls.width and sprite.y < bullet.y + Bullet.height and bullet.y < sprite.y + cls.height:
+                    cls.sprites.remove(sprite)
+                    Bullet.sprites.remove(bullet)
+                    Explosion.append(sprite.x, sprite.y)
                     break
 
             player_center_x = Player.player.x + Player.width // 2
             player_center_y = Player.player.y + Player.height // 2
-            bomb_center_x = bomb.x + Bomb.width // 2
-            bomb_center_y = bomb.y + Bomb.height // 2
-            #if bomb.x < Player.player.x + Player.width and Player.player.x < bomb.x + Bomb.width and bomb.y < Player.player.y + Player.height and Player.player.y < bomb.y + Bomb.height:
-            if (player_center_x - bomb_center_x)**2 + (player_center_y - bomb_center_y)**2 < 10**2:
+            sprite_center_x = sprite.x + cls.width // 2
+            sprite_center_y = sprite.y + cls.height // 2
+            # if sprite.x < Player.player.x + Player.width and Player.player.x < sprite.x + cls.width and sprite.y < Player.player.y + Player.height and Player.player.y < sprite.y + cls.height:
+            if (player_center_x - sprite_center_x)**2 + (player_center_y - sprite_center_y)**2 < 10**2:
                 App.game_mode = 2
-                if bomb in Bomb.bombs:
-                    Bomb.bombs.remove(bomb)
-                Explosion.append_explosion(bomb.x, bomb.y)
+                if sprite in cls.sprites:
+                    cls.sprites.remove(sprite)
+                Explosion.append(sprite.x, sprite.y)
 
-        Bomb.frame += 1
-
-    @classmethod
-    def draw_all(cls):
-        for bomb in Bomb.bombs:
-            bomb.__draw()
+        cls.frame += 1
 
 
-class Explosion:
-    explosions = deque()
+class Explosion(Sprite):
     count = 0
     width = 16
     height = 16
@@ -320,29 +306,24 @@ class Explosion:
         self.frame = 0
         self.animation_frame = 0
 
-    def __update(self):
+    def update(self):
         self.animation_frame = self.frame // 3
         self.frame += 1
 
-    def __draw(self):
+    def draw(self):
         pyxel.blt(x=self.x, y=self.y, img=0,
                   u=56, v=Explosion.height*self.animation_frame, w=Explosion.width, h=Explosion.height, colkey=5)
 
     @classmethod
-    def append_explosion(cls, x, y):
-        Explosion.explosions.append(Explosion(x=x, y=y))
+    def append(cls, x, y):
+        cls.sprites.append(cls(x=x, y=y))
 
     @ classmethod
     def update_all(cls):
-        for explosion in Explosion.explosions.copy():
-            explosion.__update()
-            if explosion.animation_frame > 3:
-                Explosion.explosions.popleft()
-
-    @ classmethod
-    def draw_all(cls):
-        for explosion in Explosion.explosions:
-            explosion.__draw()
+        for sprite in cls.sprites.copy():
+            sprite.update()
+            if sprite.animation_frame > 3:
+                cls.sprites.popleft()
 
 
 class App:
@@ -367,9 +348,11 @@ class App:
 
         # Other
         Rainbow.setup()
+        Star.setup()
         Coin.setup()
         Bomb.setup()
         Bullet.setup()
+        Explosion.setup()
 
     def update(self):
         Star.update_all()
@@ -391,8 +374,10 @@ class App:
             App.start_frame_count = pyxel.frame_count
 
         # End
-        #if pyxel.btnp(pyxel.KEY_E):
-        #    App.game_mode = 2
+        """
+        if pyxel.btnp(pyxel.KEY_E):
+           App.game_mode = 2
+        """
 
         # Quit
         if pyxel.btnp(pyxel.KEY_Q):
@@ -410,7 +395,8 @@ class App:
         Explosion.draw_all()
         Player.draw()
 
-        pyxel.text(0, 0, f"High Score:{App.high_score}", [9, 10, 7][pyxel.frame_count // 2 % 3])
+        pyxel.text(0, 0, f"High Score:{App.high_score}", [
+                   9, 10, 7][pyxel.frame_count // 2 % 3])
         pyxel.text(0, 7, f"Score:{App.score}", 13)
 
         if App.game_mode == 0:
